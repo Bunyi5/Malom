49880d29c031a4dde84eadfa98a76a00d5b19d87
Bunyi5 <bbakos262@gmail.com>
2020-04-10 20:52:19 +0200

    Created main and launch screen

---
+++ src/main/java/Controller/LaunchController.java
@@ -0,0 +1,21 @@
+package Controller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LaunchController {
+
+    @FXML
+    private TextField player1TextField;
+
+    @FXML
+    private TextField player2TextField;
+
+
+    public void startAction(ActionEvent actionEvent) {
+// TODO: 2020. 04. 10. Redirect to the game, set error label
+    }
+
+}
---
+++ src/main/java/Main/MyApplication.java
@@ -0,0 +1,20 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/launch.fxml"));
+        primaryStage.setTitle("Malom");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+}
