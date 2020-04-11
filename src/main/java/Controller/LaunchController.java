b53824c24497dbc2de307f239385be2e5463907e
Bunyi5 <bbakos262@gmail.com>
2020-04-11 18:50:49 +0200

    Added pictures, fix errorLabel at launch screen

--- src/main/java/Controller/LaunchController.java
+++ src/main/java/Controller/LaunchController.java
@@ -3,6 +3,7 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class LaunchController {
@@ -13,9 +14,18 @@ public class LaunchController {
     @FXML
     private TextField player2TextField;
 
+    @FXML
+    private Label errorLabel1;
 
     public void startAction(ActionEvent actionEvent) {
-// TODO: 2020. 04. 10. Redirect to the game, set error label
+
+        if (player1TextField.getText().isEmpty()) {
+            errorLabel1.setText("Player1 is empty!");
+        } else if (player2TextField.getText().isEmpty()) {
+            errorLabel1.setText("Player2 is empty!");
+        } else {
+
+        }
     }
 
 }
