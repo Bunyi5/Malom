c4465bbf9d11ab70d882c7dfc180a0b5db358d3e
Bunyi5 <bbakos262@gmail.com>
2020-04-24 17:15:24 +0200

    Added scm section in pom.xml, created tables in toplist.fxml, created back method in TopListController.java

--- src/main/java/Controller/TopListController.java
+++ src/main/java/Controller/TopListController.java
@@ -1,4 +1,24 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class TopListController {
+
+    public void back(ActionEvent actionEvent) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/launch.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setX((Screen.getPrimary().getBounds().getWidth()/2)-300);
+        stage.setY((Screen.getPrimary().getBounds().getHeight()/2)-250);
+        stage.show();
+    }
 }
